e594e1d8ce80588f64bd9e87b6a0779cca1c2ab2
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-09-04 14:04:44 +0100

    𝚃𝙾𝙶𝙴-𝙱𝚄𝙶-𝚅𝟸

--- settings.js
+++ settings.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "6283833304947"
+global.ownernomer = "24105114159"
 global.ownername = "𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸"
 global.ytname = "kenzo3146"
 global.socialm = "GitHub: toge012345"
 global.location = "Africa, Gabon, Libreville"
 
-global.ownernumber = '6283833304947'  //creator number
+global.ownernumber = '24105114159'  //creator number
 global.ownername = '𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸' //owner name
 global.botname = '𝚃𝙾𝙶𝙴-𝙱𝚄𝙶-𝚅𝟸' //name of the bot
 
