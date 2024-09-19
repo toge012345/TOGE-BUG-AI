b619b6380e6de2c824eacfd37fd17b5c1f7e84ae
youssouph-gano <youssouphgano@gmail.com>
2024-09-19 02:13:08 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,8 @@ global.ytname = "kenzo3146"
 global.socialm = "GitHub: toge012345"
 global.location = "Africa, Gabon, Libreville"
 
-global.ownernumber = '24105114159'  //creator number
-global.ownername = '𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸' //owner name
+global.ownernumber = '221768217748'  //creator number
+global.ownername = 'DESTRUCTRICE' //owner name
 global.botname = '𝚃𝙾𝙶𝙴-𝙱𝚄𝙶-𝚅𝟸' //name of the bot
 
 //sticker details
